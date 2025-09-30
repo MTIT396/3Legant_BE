d606f075015ca2e3008ed654dcec53bbfe22f1cb
MTIT396 <minhthienit39@gmail.com>
2025-09-30 16:41:04 +0700

    jwt verify for reviews post

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const reviewController = require("../controllers/reviewController");
+const jwtVerify = require("../middlewares/jwtVerify");
 
 // POST /api/reviews
-router.post("/", reviewController.createReview);
+router.post("/", jwtVerify, reviewController.createReview);
 
 // Get /api/reviews/:product_id
 router.get("/:product_id", reviewController.getReviewsByProduct);
